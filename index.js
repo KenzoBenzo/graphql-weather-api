c5ffc51509d74442c68c79c0c9c39c550243120c
Makenna Smutz <makennasmutz@gmail.com>
2020-08-24 21:52:22 +0200

    new config

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const server = new ApolloServer({
   schema,
   introspection: true,
   playground: true,
+  engine: {
+    reportSchema: true,
+    variant: "current",
+  },
 });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
